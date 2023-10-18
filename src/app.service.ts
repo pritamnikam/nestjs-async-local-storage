d1088baf738900047bd45e28463ec291341c6736
Pritam Nikam <pritam.nikam@gmail.com>
2023-10-18 22:10:42 +0530

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { AsyncLocalStorage } from 'async_hooks';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly als: AsyncLocalStorage<any>) {}
+
+  getHello(): string {
+    // return 'Hello World!';
+    return this.als.getStore();
+  }
+}
